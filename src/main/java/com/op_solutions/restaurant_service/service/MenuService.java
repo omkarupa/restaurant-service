15e84bdd65b471cfdb2761077fe7e3ffa12b5b21
Omkar Upadhyay <omkarmh009@gmail.com>
2026-02-15 09:17:10 +0530

    Added Caching for MenuItems search

--- src/main/java/com/op_solutions/restaurant_service/service/MenuService.java
+++ src/main/java/com/op_solutions/restaurant_service/service/MenuService.java
@@ -2,6 +2,7 @@ package com.op_solutions.restaurant_service.service;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.op_solutions.restaurant_service.dtos.MenuItemResponse;
@@ -15,15 +16,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MenuService {
 	
+	
 	private final MenuItemRepository menuItemRepository;
 
+	@Cacheable(value = "menuItems")
 	public List<MenuItemResponse> getAllAvailableItems() {
-		
+		 System.out.println("Fetching from DB...");
 		return menuItemRepository.findAll().stream().map(this::toResponse).toList();
 	}
 
+	@Cacheable(value = "menuItem", key = "#menuItemId")
 	public MenuItemResponse getMenuItem(Long menuItemId) {
-		
+		System.out.println("Fetching from DB...");
 		MenuItem item =  menuItemRepository.findById(menuItemId).orElseThrow(() -> new ResourceNotFoundException("Menu Item not found") );
 		
 		return toResponse(item);
